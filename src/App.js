d6d9fad9506cc3eb9b10b0e0c16e40476bd7e231
Mitusha Arya <aryamitusha@gmail.com>
2021-01-21 18:39:46 +0530

    Importing the URL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import Loading from './Loading';
-import Tour from './Tour';
-import Tours from './Tours'
+import Tours from './Tours';
+
+//Importing the URL
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
   return (
    <Loading> </Loading>
   );
 }
-export default App
+export default App;
