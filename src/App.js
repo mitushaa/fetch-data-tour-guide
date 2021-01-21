13c8fe50038e576a39788ea18a8e3ebc85ce0296
Mitusha Arya <aryamitusha@gmail.com>
2021-01-21 18:37:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Loading from './Loading';
+import Tour from './Tour';
+import Tours from './Tours'
+
+function App() {
+  return (
+   <Loading> </Loading>
+  );
+}
+export default App
